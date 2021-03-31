a19c347ef34214bb07ca641013ac6078c7998d4d
DiezelRush <ultramistik99@gmail.com>
2021-03-31 15:49:49 +0300

    add zadanie 11(not work)

--- delete.sh
+++ delete.sh
@@ -3,9 +3,27 @@
 #ARCHIV="$FILENAME".gz
 #FILENAME="$1"
 
+ls -li $1 | awk '{print $1}' > /tmp/inode.txt
+INODE=`sudo cat /tmp/inode.txt`
+find /home/ -inum $INODE > /tmp/inodefind.txt
+i=`sudo cat /tmp/inodefind.txt`
+
 if [ -d ~/TRASH/ ]
-then echo
-else echo "no dir"
-mkdir -p TRASH
+	then echo
+	else echo "no dir"
+		mkdir -p TRASH
 fi
-gzip -c $1 > ~/TRASH/$1.gz && rm $1
+if [[ -h $1 ]]
+	then
+		readlink $1 
+		rm $1
+		echo "symlink del"
+elif [[ ! $i -eq 1 ]]
+	then rm $1
+		echo "hardlink del"
+	fi
+     else gzip -c $1 > ~/TRASH/$1.gz && rm $1
+echo "exit"
+fi
+
+
